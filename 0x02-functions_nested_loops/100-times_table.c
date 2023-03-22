dcbc5722c17b5ffe5e082608b37a338dfea7aeb9
Shadkoech <shadrackipruto@gmail.com>
2023-03-22 08:45:06 +0000

    Multiplication-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,42 +5,44 @@
   */
 void print_times_table(int n)
 {
-int i, j, k;
+	int a, b, c;
 
-if (n >= 0 && n <= 15)
-{
-	for (i = 0; i <=n; i++)
+	if (n >= 0 && n <= 14)
 	{
-		for (j = 0; j <= n; j++)
+		for (a = 0; a <= n; a++)
 		{
-			k = j * i;
-			if (j == 0)
+			for (b = 0; b <= n; b++)
 			{
-				_putchar(k + '0');
-			} else if (k < 10 && j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			} else if (k >= 10 && k < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			} else if (k >= 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 100) + '0');
-				_putchar(((k / 100 % 10) + '0'));
-				_putchar((k % 10) + '0');
+				c = a * b;
+				if (c > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				else if (c > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				else
+				{
+					if (b != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(c + '0');
+				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
-}
 }
