6cf6da59bf6d2bae8172593fde0722fc68d850be
Shadkoech <shadrackipruto@gmail.com>
2023-03-27 13:15:58 +0000

    0. 98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *reset_to_98 -  takes a pointer to an int and updates the value
+  *@n: the value pointed to
+  *
+  *Return: Always 0
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+0x05. C - Pointers, arrays and strings
+Introduction into the concept of pointers, arrays and strings and the ways of manipulating them
