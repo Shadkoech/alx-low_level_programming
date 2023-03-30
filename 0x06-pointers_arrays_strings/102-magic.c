430849deb9c9b1766b2b5ef35d7de66a86fda998
Shadkoech <shadrackipruto@gmail.com>
2023-03-30 10:34:12 +0000

    Dream and magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+  *main - prints out magic numbers
+  *
+  *Return: Always 0
+  */
 int main(void)
 {
 	int n;
---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  *infinite_add - This is a function that adds two numbers
+  *@n1: the first number
+  *@n2: The second number
+  *@r: The buffer that the function will use to store the result
+  *@size_r: is the buffer size
+  *
+  *Return: Always 0
+  */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
