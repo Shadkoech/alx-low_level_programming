53a0732a509b80035613393ece12bb769e8019aa
Shadkoech <shadrackipruto@gmail.com>
2023-04-17 13:11:23 +0000

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  *main - gives the name of the file the code was compiled from
+  *
+  *Return: Always 0
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
