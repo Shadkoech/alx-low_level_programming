569f89558451256c0d83170983ec5d6829ea8876
Shadkoech <shadrackipruto@gmail.com>
2023-04-17 22:39:19 +0000

    poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+0x0E. C - Structures, typedef
+
+This directory holds assignements and project on how to declare and use structures, typedefs 
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+  *struct dog - Dog's key attributes
+  *@name: 1st member
+  *@age: 2nd member
+  *@owner: 3rd member
+  *
+  *Description: Dogs's basic information
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
