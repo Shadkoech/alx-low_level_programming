dad434297ef4c34235ddb46bd1a56077850dd8a9
Shadkoech <shadrackipruto@gmail.com>
2023-04-27 19:04:59 +0000

    tail node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,4 +9,32 @@
   */
 list_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *newnode;
+	list_t *temp = *head;
+	unsigned int len;
 
+	len = 0;
+	while (str[len])
+		len++;
+
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->str = strdup(str);
+	newnode->len = len;
+	newnode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (newnode);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = newnode;
+	return (newnode);
+}
