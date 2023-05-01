cbe47ca828f2b1d78b88f4eec2b672d8298d49f2
Shadkoech <shadrackipruto@gmail.com>
2023-05-01 07:24:59 +0000

    Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -26,7 +26,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (pos < index - 1)
 	{
-		if (!temp || !(temp->next))
+		if (temp == NULL || temp->next == NULL)
 			return (-1);
 		temp = temp->next;
 		pos++;
